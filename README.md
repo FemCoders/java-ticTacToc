e85becf37ce5830fc0dcdaa2dbb56d2d68f8e590
MAlexGG <alexandragalarzag@gmail.com>
2024-07-08 17:35:16 +0200

    tic tac toe briefing

---
+++ README.md
@@ -0,0 +1,27 @@
+# :video_game: Tic-Tac-Toe en Java
+
+### Introducción
+Este proyecto es una implementación del clásico juego de Tic-Tac-Toe (Tres en raya) en Java. El objetivo es practicar la programación orientada a objetos, el manejo de arrays (multidimensionales) y la lógica de control de flujo (bucles anidados). El juego se juega en la terminal, donde dos jugadores (X y O) se turnan para ingresar sus movimientos.
+
+### Instrucciones
+1. Con el link del reto que te hemos enviado por github classroom, la primera integrante del equipo aceptará la tarea y creará un equipo en el botón "create team"
+2. La segunda y tercera integrante, con el mismo link que les hemos enviado, se unirá al equipo creado por su compañera en el apartado "Join an existing team"
+3. git clone <repositorio>
+4. Lee atentamente las instrucciones indicadas abajo
+
+### Reglas del Juego
+- El juego se juega en una cuadrícula de 3x3.
+- Dos jugadores se turnan para colocar sus marcas (X o O) en una de las celdas vacías.
+- El primer jugador en alinear tres de sus marcas en una fila, columna o diagonal gana.
+- Si todas las celdas se llenan y ninguno de los jugadores ha alineado tres marcas, el juego termina en empate.
+
+### Funcionalidades
+- Muestra el estado actual del tablero después de cada movimiento.
+- Los jugadores X y O se alternan para hacer sus movimientos.
+-Pide al usuario que ingrese la fila y columna para colocar su marca y verifica que la celda esté vacía.
+- Comprueba después de cada movimiento si hay un ganador.
+- Muestra un mensaje quien ha ganado o si hay un empate.
+
+### Buenas prácticas
+Asegúrate de usar la menor cantidad de memoria posible.
+Sigue las convenciones de buenas prácticas en Java.
---
+++ TicTacToe.java
@@ -0,0 +1,91 @@
+public class TicTacToe {
+  public static void main(String[] args) {
+
+    // Crea aquí la lógica de tu programa, y ejecuta cuando requieras los métodos
+    // que has de desarrollar basándote en la documentación (docComments)
+  }
+
+  /**
+   * Function name: printBoard
+   * 
+   * @param board (char[][])
+   * 
+   *              Inside the function:
+   *              1. print a new line.
+   *              2. print the board.
+   *              • separate each row by two lines.
+   *              • each row precedes a tab of space
+   *              • each character in the grid has one space from the other
+   *              character
+   */
+
+  /**
+   * Function name: askUser
+   * 
+   * @param board (char[][])
+   * @return spot (int[])
+   * 
+   *         Inside the function
+   *         1. Asks the user: - pick a row and column number:
+   *         2. Check if the spot is taken. If so, let the user choose again.
+   *         3. Return the row and column in an int[] array.
+   */
+
+  /**
+   * Function name: checkWin
+   * 
+   * @param board (char[][])
+   * @return count (int)
+   * 
+   *         Inside the function:
+   *         1. Call checkRows
+   *         2. Call checkColumns
+   *         3. Call checkLeft
+   *         4. Call checkRight
+   *         5. If count is 3 or -3 return count
+   */
+
+  /**
+   * Function name: checkRows
+   * 
+   * @param board (char[][])
+   * @return count (int)
+   * 
+   *         Inside the function:
+   *         1. Make a count variable that starts at 0.
+   *         2. Check every row for a straight X or straight O
+   */
+
+  /**
+   * Function name: checkColumns
+   * 
+   * @param board (char[][])
+   * @return count (int)
+   * 
+   *         Inside the function:
+   *         1. Make a count variable that starts at 0.
+   *         2. Check every column for a straight X or straight O
+   */
+
+  /**
+   * Function name: checkLeft
+   * 
+   * @param board (char[][])
+   * @return count (int)
+   * 
+   *         Inside the function:
+   *         1. Make a count variable that starts at 0.
+   *         2. Check the left diagonal for a straight X or straight O
+   */
+
+  /**
+   * Function name: checkRight
+   * 
+   * @param board (char[][])
+   * @return count
+   *         Inside the function:
+   *         1. Make a count variable that starts at 0.
+   *         2. Check the right diagonal for a straight X or straight O
+   */
+
+}
